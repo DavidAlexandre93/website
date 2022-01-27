d4339d1486eec1379732ba6ff8712fbdde2c392f
David Alexandre Fernandes <davidalexandrefernandes@outlook.com>
2022-01-27 13:53:58 -0300

    img3

--- index.php
+++ index.php
@@ -269,7 +269,7 @@ $msg = $_REQUEST['msg'];*/
         </div>
 
         <div class="shadow-large" data-date="Junho 2020 – Maio 2021">
-        <div>
+        <div class="project-image" Style="height: 50px; width: 50px">
             <img src="images/hortelan.jpg" />
         </div>
               <h3>Banco Itaú Unibanco</h3>
