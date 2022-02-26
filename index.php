9072a81df2ea86cc8bcef5753e5050cedbf017d3
David Alexandre Fernandes <davidalexandrefernandes@outlook.com>
2022-02-26 06:11:36 -0300

    rastreamentoweb

--- index.php
+++ index.php
@@ -668,7 +668,7 @@ $msg = $_REQUEST['msg'];*/
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/scripts.min.js"></script>
 
-<!-- clicky analictys para rastrear site -->
+<!-- Clicky Analytics para rastrear site -->
 <a title="Real Time Web Analytics" href="http://clicky.com/101355530"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
 <script async src="//static.getclicky.com/101355530.js"></script>
 <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101355530ns.gif" /></p></noscript>
