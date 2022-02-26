6d01558adbf9aaf5a13a27a9e5edf2cc4c1d649d
David Alexandre Fernandes <davidalexandrefernandes@outlook.com>
2022-02-26 06:08:27 -0300

    rastreamento

--- index.php
+++ index.php
@@ -669,9 +669,9 @@ $msg = $_REQUEST['msg'];*/
     <script src="js/scripts.min.js"></script>
 
 <!-- clicky analictys para rastrear site -->
-<a title="Real Time Web Analytics" href="http://clicky.com/101355529"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
-<script async src="//static.getclicky.com/101355529.js"></script>
-<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101355529ns.gif" /></p></noscript>
+<a title="Real Time Web Analytics" href="http://clicky.com/101355530"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+<script async src="//static.getclicky.com/101355530.js"></script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101355530ns.gif" /></p></noscript>
  
 </body>
 <!-- FIM DO BODY -->
